015b782fed3520d30f29ed66be7b00d70bd4da9c
levinceotieno <levynceotieno@gmail.com>
2023-06-15 17:55:54 +0300

    this commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - printing without printf or puts
+ * output to stdErr
+ * return 1
+*/
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
