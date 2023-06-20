cf7c88051271b2fa81870c97e87292ca8fd88fc7
levinceotieno <levynceotieno@gmail.com>
2023-06-20 16:40:49 +0300

    my commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,12 +5,11 @@
  * Return: 0 (Success)
 */
 void jack_bauer(void)
-/* function implemantation */
 {
 int m, n;
 for (m = 0; m < 24; m++)
 {
-while (n < 60)
+for (n = 0; n < 60; n++)
 {
 _putchar(m / 10 + '0');
 _putchar(m % 10 + '0');
@@ -18,7 +17,6 @@ _putchar(':');
 _putchar(n / 10 + '0');
 _putchar(n % 10 + '0');
 _putchar('\n');
-n++;
 }
 }
 }
