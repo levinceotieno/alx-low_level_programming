4ed675fa77eda2a3d0487332c5253deb99399754
levinceotieno <levynceotieno@gmail.com>
2023-06-20 17:21:28 +0300

    my commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - it adds two integers and return result
+ * @x: int added to int z
+ * @z: int added to in x
+ * Return: (sum)
+*/
+int add(int x, int z)
+{
+int y;
+y = x + z;
+return (y);
+}
