cb6e228db358e2f7e24d81ab9d4ecdc7e97fbb50
levinceotieno <levynceotieno@gmail.com>
2023-07-10 17:28:29 +0300

    an attempt

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -4,7 +4,7 @@
 /**
  * count_word - it is spliting a string into words...
  * @s: this is the Stringg
- * 
+ *
  * Return: NULL if str == NULL or str == ""
  */
 int count_word(char *s)
@@ -32,7 +32,7 @@ return (z);
 /**
  * **strtow - it Splits a string down to words...
  * @str: String to be splitted
- * 
+ *
  * Return: pointer to an array of strings or NULL (Error)
  */
 
@@ -42,20 +42,17 @@ char **mmaattrriixx_, *shows_tmp;
 int k;
 int j = 0;
 int string_length = 0;
-int f = 0; 
+int f = 0;
 int the_words, Begginn, _ending;
 
 while (*(str + string_length))
 string_length++;
 the_words = count_word(str);
-
 if (the_words == 0)
 return (NULL);
-
 mmaattrriixx_ = (char **) malloc(sizeof(char *) * (the_words + 1));
 if (mmaattrriixx_ == NULL)
 return (NULL);
-
 for (k = 0; k <= string_length; k++)
 {
 if (str[k] == ' ' || str[k] == '\0')
@@ -66,7 +63,6 @@ _ending = k;
 shows_tmp = (char *) malloc(sizeof(char) * (f + 1));
 if (shows_tmp == NULL)
 return (NULL);
-
 while (Begginn < _ending)
 *shows_tmp++ = str[Begginn++];
 *shows_tmp = '\0';
@@ -75,11 +71,9 @@ j++;
 f = 0;
 }
 }
-
 else if (f++ == 0)
 Begginn = k;
 }
-
 mmaattrriixx_[j] = NULL;
 return (mmaattrriixx_);
 }
