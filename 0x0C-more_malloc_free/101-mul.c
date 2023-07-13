0762dbba53ab3e1d5ee56eda7881debdb2038d70
levinceotieno <levynceotieno@gmail.com>
2023-07-13 15:05:32 +0300

    last commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,110 +1,113 @@
-#include <stdio.h>
 #include "main.h"
-#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
 /**
- * _is_zero - checking if any NUMBER is 0
- * @argv: argu v...
- * Return: void
+ * _is_zero - entry...looks for 0
+ * @argv: vecto,,,
+ * Return: (void)
  */
+
 void _is_zero(char *argv[])
 {
-int w, var_1 = 1, var_2 = 1;
-for (w = 0; argv[1][w]; w++)
-if (argv[1][w] != '0')
+int k, m = 1, n = 1;
+for (k = 0; argv[1][k]; k++)
+if (argv[1][k] != '0')
 {
-var_1 = 0;
+m = 0;
 break;
 }
-for (w = 0; argv[2][w]; w++)
-if (argv[2][w] != '0')
+for (k = 0; argv[2][k]; k++)
+if (argv[2][k] != '0')
 {
-var_2 = 0;
+n = 0;
 break;
 }
-if (var_1 == 1 || var_2 == 1)
+if (m == 1 || n == 1)
 {
 printf("0\n");
 exit(0);
 }
 }
 /**
- * _initialize_array - set memery to zero in a new array
- * @ar: char array.
- * @lar: length of the char array.
- * Return: pointer of a char array.
+ * _initialize_array - it is setting the new memory = 0
+ *
+ * @lar: LENGTH..
+ * @ar: ARRAY
+ * Return: POINTER (Success)
  */
 char *_initialize_array(char *ar, int lar)
 {
-int i = 0;
-for (i = 0; i < lar; i++)
-ar[i] = '0';
+int v = 0;
+for (v = 0; v < lar; v++)
+ar[v] = '0';
 ar[lar] = '\0';
 return (ar);
 }
 /**
- * _checknum - Checking the len of NUMBER...& base 10
- * @argv: argu...v
- * @n: Num of arrays
- * Return: lngth...
+ * _checknum - LENGTH & base 10 of number
+ * @argv: vecto,,,
+ * @n: ARRAY NUMBER
+ * Return: LENGTH
  */
 int _checknum(char *argv[], int n)
 {
-int lngth;
-for (lngth = 0; argv[n][lngth]; lngth++)
-if (!isdigit(argv[n][lngth]))
+int lennn;
+for (lennn = 0; argv[n][lennn]; lennn++)
+if (!isdigit(argv[n][lennn]))
 {
 printf("Error\n");
 exit(98);
 }
-return (lngth);
+return (lennn);
 }
 /**
- * main - it multiplies 2 +ve numbers...
- * @argc: arg count...
- * @argv: arg vector...
- * Return: Always 0...
+ * main - 2 +ve numbss being multiplied
+ * @argc: number of counts
+ * @argv: vecto,,,
+ * Return: 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-int x_1, x_2, s, sum_1, sum_en, i, j, k, ca;
-char *ar_;
+int r1, r2, s, plus, LH, y1, y22, y333, randmm;
+char *str;
 
 if (argc != 3)
 printf("Error\n"), exit(98);
-x_1 = _checknum(argv, 1), x_2 = _checknum(argv, 2);
-_is_zero(argv), s = x_1 + x_2, ar_ = malloc(s + 1);
-if (ar_ == NULL)
+r1 = _checknum(argv, 1), r2 = _checknum(argv, 2);
+_is_zero(argv), s = r1 + r2, str = malloc(s + 1);
+if (str == NULL)
 printf("Error\n"), exit(98);
-ar_ = _initialize_array(ar_, s);
-k = s - 1, i = x_1 - 1, j = x_2 - 1, ca = sum_en = 0;
-for (; k >= 0; k--, i--)
+str = _initialize_array(str, s);
+y333 = s - 1, y1 = r1 - 1, y22 = r2 - 1, randmm = LH = 0;
+for (; y333 >= 0; y333--, y1--)
 {
-if (i < 0)
+if (y1 < 0)
 {
-if (sum_en > 0)
+if (LH > 0)
 {
-sum_1 = (ar_[k] - '0') + sum_en;
-if (sum_1 > 9)
-ar_[k - 1] = (sum_1 / 10) + '0';
-ar_[k] = (sum_1 % 10) + '0';
+plus = (str[y333] - '0') + LH;
+if (plus > 9)
+str[y333 - 1] = (plus / 10) + '0';
+str[y333] = (plus % 10) + '0';
 }
-i = x_1 - 1, j--, sum_en = 0, ca++, k = s - (1 + ca);
+y1 = r1 - 1, y22--, LH = 0, randmm++, y333 = s - (1 + randmm);
 }
-if (j < 0)
+if (y22 < 0)
 {
-if (ar_[0] != '0')
+if (str[0] != '0')
 break;
 s--;
-free(ar_), ar_ = malloc(s + 1), ar_ = _initialize_array(ar_, s);
-k = s - 1, i = x_1 - 1, j = x_2 - 1, ca = sum_en = 0;
+free(str), str = malloc(s + 1), str = _initialize_array(str, s);
+y333 = s - 1, y1 = r1 - 1, y22 = r2 - 1, randmm = LH = 0;
 }
-if (j >= 0)
+if (y22 >= 0)
 {
-sum_1 = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (ar_[k] - '0') + sum_en;
-sum_en = sum_1 / 10, ar_[k] = (sum_1 % 10) + '0';
+plus = ((argv[1][y1] - '0') * (argv[2][y22] - '0')) + (str[y333] - '0') + LH;
+LH = plus / 10, str[y333] = (plus % 10) + '0';
 }
 }
-printf("%s\n", ar_);
+printf("%s\n", str);
 return (0);
 }
