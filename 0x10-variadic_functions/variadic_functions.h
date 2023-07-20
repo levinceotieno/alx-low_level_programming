5e51d23b25ddbba5ff99ce3ecb2f5c88d8882a48
levinceotieno <levynceotieno@gmail.com>
2023-07-20 15:30:22 +0300

    update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,6 @@
 #ifndef VARI_F_H
 #define VARI_F_H
-#include <stdarg.h>
+
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
